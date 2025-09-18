97fb00349d6ce664d993384d6b80cb484710ac4f
simha730 <sambangisimha@gmail.com>
2025-09-18 10:28:10 +0530

    Update same tree.c

--- same tree.c
+++ same tree.c
@@ -7,10 +7,7 @@
  * };
  */
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-  /* if(p==NULL&&q==NULL) return true;//true
-if(p==NULL||q==NULL) return false;
-if(p->val!=q->val) return false;
-return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);*/
+
 if(p==NULL&&q==NULL)return true;
 if(p==NULL||q==NULL) return false;
 if(p->val!=q->val) return false;
